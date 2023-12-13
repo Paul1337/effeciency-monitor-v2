4b74560b659b86856fb9ba293f43a10870e75d38
Paul <p@x128.ru>
2023-12-13 21:11:29 +0300

    some filtration

--- client/src/ui/components/TodayWidget/TodayWidget.tsx
+++ client/src/ui/components/TodayWidget/TodayWidget.tsx
@@ -1,4 +1,13 @@
-import { CardBody, CardHeader, Heading, Text } from '@chakra-ui/react';
+import {
+    Box,
+    CardBody,
+    CardHeader,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Text,
+} from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { IDeal } from '../../../domain/models/Deal/model';
 import { thunkAccomplishDeal } from '../../../domain/redux/services/deal/accomplishDeal';
@@ -7,12 +16,15 @@ import { RootState, useAppDispatch } from '../../../domain/redux/store';
 import { WeekdaysNames } from '../../shared/weekdays';
 import { BaseCard } from '../BaseCard/BaseCard';
 import { TodayItem } from './TodayItem';
+import { useState } from 'react';
 
 export const TodayWidget = () => {
     const dispatch = useAppDispatch();
     const dailyPlans = useSelector((state: RootState) => state.plans.dailyPlans);
     const todayHistory = useSelector((state: RootState) => state.history.today);
     const deals = useSelector((state: RootState) => state.deals.deals);
+
+    const [todayDealsFilter, setTodayDealsFilter] = useState('');
 
     const currentDate = new Date();
     const currentWeekday = (currentDate.getDay() + 6) % 7;
@@ -43,28 +55,46 @@ export const TodayWidget = () => {
             </CardHeader>
             <CardBody overflowY={'auto'}>
                 <Text>Planned deals:</Text>
-                {todayPlans.map(({ deal, weekdaysCount }, index) => (
-                    <TodayItem
-                        onDo={() => handleTodayItemDo(deal)}
-                        onUndo={() => handleTodayItemUndo(deal)}
-                        deal={deal}
-                        count={weekdaysCount[currentWeekday]}
-                        done={todayHistory?.done[deal.name] ?? 0}
-                        key={deal.name + index}
-                        canUndo={(todayHistory?.done[deal.name] ?? 0) > 0}
+                <FormControl mt={2} display={'flex'} alignItems={'center'}>
+                    <FormLabel>Filter: </FormLabel>
+                    <Input
+                        type='text'
+                        value={todayDealsFilter}
+                        onChange={e => setTodayDealsFilter(e.target.value)}
                     />
-                ))}
+                </FormControl>
+                <Box overflowY={'auto'} flex={1}>
+                    {todayPlans
+                        .filter(pl =>
+                            pl.deal.name.toLowerCase().includes(todayDealsFilter.toLocaleLowerCase())
+                        )
+                        .map(({ deal, weekdaysCount }, index) => (
+                            <TodayItem
+                                onDo={() => handleTodayItemDo(deal)}
+                                onUndo={() => handleTodayItemUndo(deal)}
+                                deal={deal}
+                                count={weekdaysCount[currentWeekday]}
+                                done={todayHistory?.done[deal.name] ?? 0}
+                                key={deal.name + index}
+                                canUndo={(todayHistory?.done[deal.name] ?? 0) > 0}
+                            />
+                        ))}
+                </Box>
                 <Text>Extra deals (not in today plan):</Text>
-                {otherDeals.map((deal, index) => (
-                    <TodayItem
-                        key={deal.name + index}
-                        deal={deal}
-                        done={todayHistory?.done[deal.name] ?? 0}
-                        onDo={() => handleTodayItemDo(deal)}
-                        onUndo={() => handleTodayItemUndo(deal)}
-                        canUndo={(todayHistory?.done[deal.name] ?? 0) > 0}
-                    />
-                ))}
+                {otherDeals
+                    .filter(deal =>
+                        deal.name.toLowerCase().includes(todayDealsFilter.toLocaleLowerCase())
+                    )
+                    .map((deal, index) => (
+                        <TodayItem
+                            key={deal.name + index}
+                            deal={deal}
+                            done={todayHistory?.done[deal.name] ?? 0}
+                            onDo={() => handleTodayItemDo(deal)}
+                            onUndo={() => handleTodayItemUndo(deal)}
+                            canUndo={(todayHistory?.done[deal.name] ?? 0) > 0}
+                        />
+                    ))}
             </CardBody>
         </BaseCard>
     );
